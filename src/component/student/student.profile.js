37dac420033b23d25a4ca574b97347aee3c1ab73
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-03-25 17:39:34 +0200

    Update student.profile.js

--- src/component/student/student.profile.js
+++ src/component/student/student.profile.js
@@ -1,9 +1,9 @@
+const bcrypt=require('bcrypt')
 const AppError = require("../../utils/AppError");
 const { catchAsyncError } = require("../../utils/catchAsyncErr");
 const { deleteFromCloudinary } = require("../../utils/cludinary");
 const StudentModel = require("./student.model");
 exports.getProfile = catchAsyncError(async (req, res, next) => {
-  console.log("test");
   if (req.query.lang == "en") {
     const Student = await StudentModel.findById(req.Student._id).populate([
       { path: "activity", select: "title_en -_id" },
@@ -35,14 +35,17 @@ exports.updateProfile = catchAsyncError(async (req, res, next) => {
     res.status(200).json({ message: "success update" });
   }
 });
-module.exports.ChangePass = catchAsyncError(async (req, res) => {
+module.exports.ChangePass = catchAsyncError(async (req, res,next) => {
+
   const { oldPassword, newPassword } = req.body;
+  console.log(oldPassword,newPassword);
   let match = await bcrypt.compare(oldPassword, req.Student.password);
   if (match) {
     let hash = await bcrypt.hash(newPassword, Number(process.env.saltRounds));
-    await StudentModel.findByIdAndUpdate(req.Student, { password: hash });
+   
+    await StudentModel.findByIdAndUpdate(req.Student._id, { password: hash });
     res.status(200).json({ message: " change password is succes" });
   } else {
-    return res.status(200).json({ message: "old password incorrect" });
+   return next(new AppError("Old password is incorrect",401))
   }
 });
