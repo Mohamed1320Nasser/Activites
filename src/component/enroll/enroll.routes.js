2624c6713ed31be287c16ea051906b9739864877
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-05-11 14:04:44 +0300

    Update enroll.routes.js

--- src/component/enroll/enroll.routes.js
+++ src/component/enroll/enroll.routes.js
@@ -3,7 +3,7 @@ const { protectedRoutes } = require("../student/student.auth");
 const { enrollActivity, cancel, enrollTrip } = require("./enroll.services");
 
 const router = require("express").Router({mergeParams: true});
-    router.post("/activity",protectedRoutes,enrollActivity)
-    router.post("/cancel",protectedRoutes,cancel)
-    router.post("/trips",protectedRoutes,enrollTrip)
+    router.get("/activity",protectedRoutes,enrollActivity)
+    router.get("/cancel",protectedRoutes,cancel)
+    router.get("/trips",protectedRoutes,enrollTrip)
 module.exports = router;
