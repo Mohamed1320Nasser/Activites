0f822586d128967ced44f13ccb64da75e3267792
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-06-09 21:29:32 +0300

    final day

--- src/component/reports/report.service.pdf.js
+++ src/component/reports/report.service.pdf.js
@@ -2,16 +2,22 @@ let pdf = require("pdf-creator-node");
 let fs = require("fs");
 let path = require("path");
 const { options } = require("./option");
-exports.createPDF= async (students)=>{
-    let html = fs.readFileSync(path.join(__dirname,"./pdf.html"), "utf8");
-    let fileName="student.pdf"
-    let route=  path.join(__dirname,"../../docs",fileName)
-    let document = {
-        html: html,
-        data: {students},
-        path: route
-      };
-      let filePath=`${process.env.DOMAIN}/${fileName}`
-   await pdf.create(document, options)
-     return { filePath , route}
+exports.createPDF = async (students) => {
+  let html = fs.readFileSync(path.join(__dirname, "./pdf.html"), "utf8");
+  let fileName = "student.pdf"
+  let route = path.join(__dirname, "../../docs", fileName)
+  let document = {
+    html: html,
+    data: { students },
+    path: route
+  };
+  let filePath = `${process.env.DOMAIN}/${fileName}`
+  await pdf.create(document, options, {
+    childProcessOptions: {
+      env: {
+        OPENSSL_CONF: '/dev/null',
+      },
     }
+  })
+  return { filePath, route }
+}
