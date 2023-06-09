5055765e111d36ef26a38d0a78c0b3692ec46f35
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-06-10 02:21:14 +0300

    final day

--- src/component/reports/report.service.pdf.js
+++ src/component/reports/report.service.pdf.js
@@ -3,6 +3,7 @@ let fs = require("fs");
 let path = require("path");
 const { options } = require("./option");
 exports.createPDF = async (students) => {
+  process.env.OPENSSL_CONF = 'NUL'
   let html = fs.readFileSync(path.join(__dirname, "./pdf.html"), "utf8");
   let fileName = "student.pdf"
   let route = path.join(__dirname, "../../docs", fileName)
@@ -21,3 +22,15 @@ exports.createPDF = async (students) => {
   })
   return { filePath, route }
 }
+  /*
+html-pdf: Unknown Error\nAuto configuration failed
+\n139764995444352:error:25066067:DSO support routines:DLFCN_LOAD:could not load the shared library
+:dso_dlfcn.c:185:filename(libssl_conf.so):
+ libssl_conf.so: cannot open shared object file: No such file or 
+ directory\n139764995444352:error:25070067
+ :DSO support routines:DSO_load:could not load the shared library
+ :dso_lib.c:244:\n139764995444352:error:0E07506E:configuration file routines:MODULE_LOAD_DSO:
+ error loading dso:conf_mod.c:285:module=ssl_conf,
+  path=ssl_conf\n139764995444352:error:0E076071:configuration file routines
+  :MODULE_RUN:unknown module name:conf_mod.c:222:module=ssl_conf\n"
+*/
